d3a5848c667a68d082aa679d76e09a801a233bf3
Agubs <agubuikecynthia936@gmail.com>
2022-09-09 08:01:43 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <studio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet < = 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+
