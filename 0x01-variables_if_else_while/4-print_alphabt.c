73648ffcab5658afbc91141ff6cde03c403d1606
Agubs <agubuikecynthia936@gmail.com>
2022-09-09 09:20:30 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,21 +5,20 @@
  * followed by a new line
  * Return: Always 0 (Succss)
  */
-
 int main(void)
-
 {
 	char ch = 'a';
-	
+
 	while (ch <= 'z')
 	{
 		if (ch != 'e' && ch != 'q')
 		{
 			putchar(ch);
-		}		}
+		}
 		ch++;
 	}
 	putchar('\n');
 	return (0);
 }
+
 
