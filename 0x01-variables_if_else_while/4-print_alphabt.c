6d8d8eaebf4977b3d05f854bd6abd9ff5bc1c64d
Agubs <agubuikecynthia936@gmail.com>
2022-09-09 09:07:43 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - prints the alphabeth in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Succss)
+ */
+
+int main(void)
+
+{
+	char ch = 'a';
+	
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+																	}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
+
