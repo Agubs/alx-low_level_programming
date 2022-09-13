72e778bd34d50b1445a518e21ab7cc537fa3d115
Agubs <agubuikecynthia936@gmail.com>
2022-09-13 08:16:35 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,15 @@ void print_alphabet_x10(void)
 {
 	int alphabet;
 	int count;
+
 	count = 0;
-	
 	while (count < 10)
 	{
 		for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
 		{
 			_putchar(alphabet);
 		}
+
 		count++;
 		_putchar('\n');
 	}
