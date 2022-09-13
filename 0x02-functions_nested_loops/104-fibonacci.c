22ed0375f2d2c287a819f7a95c3e38f5512d08e0
Agubs <agubuikecynthia936@gmail.com>
2022-09-13 15:23:07 -0700

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,10 +9,10 @@
 int main(void)
 {
 	unsigned long int i, j, k, j1, j2, k1, k2;
+
 	j = 1;
-	k = 2;
-	
-	printf("%lu", j);	
+	k = 2;	
+	printf("%lu", j);
 	for (i = 1; i < 91; i++)
 	{
 		printf(", %lu", k);
