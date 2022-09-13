4519464a338a2f1cf642b90a54664a177928ebba
Agubs <agubuikecynthia936@gmail.com>
2022-09-13 08:38:01 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - return 0 letter not lowercase, 1 letter lowercase
+ *
+ *@n: the int to print
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+}
