91667c6795e97038e80eb25cc67613f85783fa74
Agubs <agubuikecynthia936@gmail.com>
2022-09-14 07:22:13 -0700

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -14,13 +14,12 @@ int main(void)
 	i = 0;
 
 	/*
-	 * while (i < 10)
-	 * {
-	 *	putchar(i);
-	 * }
+	 *while (i < 10)
+	 *{
+	 *Infinite loop - No increase of variable
+	 *putchar(i);
+	 }
 	 */
-
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
