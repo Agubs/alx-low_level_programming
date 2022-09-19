7825ca6ec70debecfc2826275186c95607f58702
Agubs <agubuikecynthia936@gmail.com>
2022-09-19 07:23:20 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of two integers a and b
+ *@a: A Pointer to an int that will be updated
+ *@b: B Pointer to an int that will be updated
+ *Return: (0)
+ */
+
+void swap_int(int *a, int *b)
+{
+	int f;
+	f = *a;
+	*a = *b;
+	*b = f;
+}
