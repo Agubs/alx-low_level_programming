0d5eaf9d7558e1b2356c0dd55e48f99beb8444c2
Agubs <agubuikecynthia936@gmail.com>
2022-09-26 10:11:03 -0700

    Task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print the chessboard, 2d array
+ * @a: 2d array of chars
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	/*Declaring variables*/
+	int i, j;
+	while (i < 8) /*number repititions*/
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar (a[i][j]);
+			j++;
+		}
+		_putchar ('\n'); /*print new line*/
+		i++;
+	}
+}
