56bed54e289de48fde70ed9a1fdaebe3e9ab8e26
Agubs <agubuikecynthia936@gmail.com>
2022-10-03 06:59:20 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,9 +15,8 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	
 	/*Define values with malloc*/
-	buffer = (char *)malloc(size *sizeof(c));
+	buffer = (char *)malloc(size * sizeof(c));
 
 	if (buffer == 0)
 	{
