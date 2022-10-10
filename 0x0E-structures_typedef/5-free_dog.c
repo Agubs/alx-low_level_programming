2afdc202254f55c02bdeb9a518f2decdb5869ae8
Agubs <agubuikecynthia936@gmail.com>
2022-10-10 06:52:32 -0700

    Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free dog struct
+ * @d: dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
